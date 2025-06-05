3493fa2b8f460a9321fbe9f76d1063afc1c83617
oliverliu.credera <oliver.liu@credera.com>
2025-06-05 10:04:05 -0500

    Create hotSauces source

--- InternGroupProject-master/group-project-ui/src/modules/pages/home-page/home-page.component.jsx
+++ InternGroupProject-master/group-project-ui/src/modules/pages/home-page/home-page.component.jsx
@@ -3,10 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { retrieveCart } from '../../cart/retrieve-cart/retrieve-cart.slice';
 import { RequestStatus } from '../../common/redux/redux.constants';
 import { Typography, Container, Box } from '@material-ui/core';
-import heatwave_havoc from '../../../assets/heatwave_havoc.png'
-import chili_charge from '../../../assets/chili_charge.png'
-import flame_frenzy from '../../../assets/flame_frenzy.png'
-import zesty_zing from '../../../assets/zesty_zing.png'
+import hotSauces from '../../../data/hotSauces';
 import logo from '../../../assets/blaster_bomb_logo.png'
 
 export const HomePageComponent = () => {
@@ -29,72 +26,7 @@ export const HomePageComponent = () => {
         </Box>
         {/* Product cards */}
         <Box display="flex" flexWrap="wrap" justifyContent="center" style={{ gap: 16 }}>
-          { [
-            {
-              id: 1,
-              name: 'Heatwave Havoc',
-              price: '$10.50',
-              description: 'A fiery, heat bomb that is our hottest sauce--guaranteed to bring you tears.',
-              spice: 'Blazing',
-              img: heatwave_havoc,
-            },
-            {
-              id: 2,
-              name: 'Chili Charge',
-              price: '$9.70',
-              description: 'A medium level chili-infused sauce that is sure to bring some heat!',
-              spice: 'Hot',
-              img: chili_charge,
-            },
-            {
-              id: 3,
-              name: 'Flame Frenzy',
-              price: '$9.70',
-              description: 'A fun, savory hot sauce to make your mouth tingle.',
-              spice: 'Mild',
-              img: flame_frenzy,
-            },
-            {
-              id: 4,
-              name: 'Zesty Zing',
-              price: '$10.20',
-              description: 'Our version of sweet and tangy with some ZING!',
-              spice: 'Mild',
-              img: zesty_zing,
-            },
-            {
-              id: 5,
-              name: 'Spice Surge',
-              price: '$11.00',
-              description: 'An electrifying blend of spices that will awaken your taste buds.',
-              spice: 'Hot',
-              img: zesty_zing, // Change this
-            },
-            {
-              id: 6,
-              name: 'Inferno Infusion',
-              price: '$12.50',
-              description: 'A dangerously addictive sauce with an infernal kick.',
-              spice: 'Blazing',
-              img: zesty_zing, // Change this
-            },
-            {
-              id: 7,
-              name: 'Pepper Pulse',
-              price: '$9.00',
-              description: 'A vibrant peppery sauce that adds a lively pulse to your dishes.',
-              spice: 'Medium',
-              img: zesty_zing, // Change this
-            },
-            {
-              id: 8,
-              name: 'Fiery Fusion',
-              price: '$10.80',
-              description: 'A harmonious fusion of flavors with a fiery finish.',
-              spice: 'Hot',
-              img: zesty_zing, // Change this
-            }
-          ].map(product => (
+          { hotSauces.map(product => (
             <Box key={product.id} style={{ background: 'var(--jet)', color: 'var(--timberwolf)', borderRadius: 12, margin: 8, width: 420, boxShadow: '0 2px 8px rgba(43,44,40,0.08)', padding: 16, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               <img src={product.img} alt={product.name} style={{ background: 'var(--timberwolf', width: 240, height: 240, objectFit: 'contain', borderRadius: 8, marginBottom: 12, border: '2px solid var(--main)' }} />
               <Typography variant="h6" style={{ color: 'var(--main)', fontWeight: 600 }}>{product.name}</Typography>
--- InternGroupProject-master/group-project-ui/src/modules/pages/product-page/product-page.component.jsx
+++ InternGroupProject-master/group-project-ui/src/modules/pages/product-page/product-page.component.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import heatwave_havoc from '../../../assets/heatwave_havoc.png'
-import chili_charge from '../../../assets/chili_charge.png'
-import flame_frenzy from '../../../assets/flame_frenzy.png'
-import zesty_zing from '../../../assets/zesty_zing.png'
-import logo from '../../../assets/blaster_bomb_logo.png'
+import hotSauces from '../../../data/hotSauces';
 
 // Simple notification at bottom
 const BottomNotification = ({ message, isVisible }) => {
@@ -44,72 +40,7 @@ const useNotification = () => {
 };
 
 // Placeholder data for products (can be replaced with actual data fetching)
-const sampleProducts = [
-  {
-    id: 1,
-    name: 'Heatwave Havoc',
-    price: '$10.50',
-    description: 'A fiery, heat bomb that is our hottest sauce--guaranteed to bring you tears.',
-    spice: 'Blazing',
-    img: heatwave_havoc,
-  },
-  {
-    id: 2,
-    name: 'Chili Charge',
-    price: '$9.70',
-    description: 'A medium level chili-infused sauce that is sure to bring some heat!',
-    spice: 'Hot',
-    img: chili_charge,
-  },
-  {
-    id: 3,
-    name: 'Flame Frenzy',
-    price: '$9.70',
-    description: 'A fun, savory hot sauce to make your mouth tingle.',
-    spice: 'Mild',
-    img: flame_frenzy,
-  },
-  {
-    id: 4,
-    name: 'Zesty Zing',
-    price: '$10.20',
-    description: 'Our version of sweet and tangy with some ZING!',
-    spice: 'Mild',
-    img: zesty_zing,
-  },
-  {
-    id: 5,
-    name: 'Spice Surge',
-    price: '$11.00',
-    description: 'An electrifying blend of spices that will awaken your taste buds.',
-    spice: 'Hot',
-    img: zesty_zing, // Change this
-  },
-  {
-    id: 6,
-    name: 'Inferno Infusion',
-    price: '$12.50',
-    description: 'A dangerously addictive sauce with an infernal kick.',
-    spice: 'Blazing',
-    img: zesty_zing, // Change this
-  },
-  {
-    id: 7,
-    name: 'Pepper Pulse',
-    price: '$9.00',
-    description: 'A vibrant peppery sauce that adds a lively pulse to your dishes.',
-    spice: 'Medium',
-    img: zesty_zing, // Change this
-  },
-  {
-    id: 8,
-    name: 'Fiery Fusion',
-    price: '$10.80',
-    description: 'A harmonious fusion of flavors with a fiery finish.',
-    spice: 'Hot',
-    img: zesty_zing, // Change this
-  }
-];
+const sampleProducts = hotSauces;
 
 const ProductsPageComponent = () => {
   const { notification, showNotification } = useNotification();
